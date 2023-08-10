1d797949fd633586967440de7a861d4b5d52d3c8
Kennedy Daniel <kenidankid@yahoo.com>
2023-08-10 20:55:00 +0100

    created at for user

--- src/model/user.js
+++ src/model/user.js
@@ -1,28 +1,30 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String, default: null },
+    last_name: { type: String, default: null },
+    email: { type: String, unique: true },
+    hash_password: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const userSchema = new mongoose.Schema({
-  first_name: { type: String, default: null },
-  last_name: { type: String, default: null },
-  email: { type: String, unique: true },
-  hash_password: { type: String },
+userSchema.virtual("password").set(function (password) {
+  this.hash_password = bcrypt.hashSync(password, 10);
 });
 
-userSchema.virtual('password')
-.set(function(password){
-    this.hash_password = bcrypt.hashSync(password, 10)
-})
-
-userSchema.virtual('fullName')
-.get(function(){
-    return `${this.firstName} ${this.lastName}`
-})
+userSchema.virtual("fullName").get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
 
 userSchema.methods = {
-    authenticate:  function(password){
-        return  bcrypt.compare(password, this.hash_password)
-    }
-}
+  authenticate: function (password) {
+    return bcrypt.compare(password, this.hash_password);
+  },
+};
 
 module.exports = mongoose.model("User", userSchema);
